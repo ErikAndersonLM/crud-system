decd407cba2f572ec9597e600f037629c1505ced
Erik <erikanderson.lm@gmail.com>
2024-07-27 15:13:07 -0300

    add register page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,51 @@
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
 export default function SignUp() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return(
-        <div>
-            <h1>Pagina Cadastro</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='Logo do sistema de cadastro.' />
+                </div>
+
+                <form>
+                    <h1>Cadastre-se</h1>
+
+                    <input
+                     type='text'
+                     placeholder='Seu nome'
+                     value={name}
+                     onChange={ (e) => setEmail(e.target.value)}   
+                    />
+
+                    <input
+                     type='text'
+                     placeholder='email@email.com'
+                     value={email}
+                     onChange={ (e) => setEmail(e.target.value)}   
+                    />
+
+                    <input
+                     type='password'
+                     placeholder='*********'
+                     value={password}
+                     onChange={ (e) => setPassword(e.target.value)}   
+                    />
+                    
+                    <button type="submit">Criar Conta</button>                
+                    
+                    </form>
+
+                <Link to="/">Já possui uma conta? Faça login</Link>
+
+            </div>
         </div>
     )
 }
